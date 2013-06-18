ad8f57fed422728a760d0657145b05b7d50dcb2a
Xuncheng Wang <w.xun.cheng@gmail.com>
2013-06-18 10:32:53 +0800

    Improve password digest error message

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  attr_accessible :remember_token
   has_many :posts
   has_many :comments
 
@@ -10,8 +11,9 @@ class User < ActiveRecord::Base
   validates :email, presence: true,
                     format: {with: VALID_EMAIL_REGEX},
                     uniqueness: {case_sensitive: false}
-  validates :password, length: {minimum: 6}
+  validates :password, presence: true, length: {minimum: 6}
   validates :password_confirmation, presence: true
+  after_validation {self.errors.messages.delete(:password_digest)}
 
   private
   def create_remember_token
