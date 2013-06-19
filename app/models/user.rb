ca594f4f97954a62726228213abbe3cf3de6e8e4
Xuncheng Wang <w.xun.cheng@gmail.com>
2013-06-19 23:38:05 +0800

    Add user name uniqueness validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
   has_secure_password
   before_save {|user| user.email = email.downcase}
   before_save :create_remember_token
-  validates :username, presence: true, length: {maximum: 50}
+  validates :username, presence: true, length: {maximum: 50}, uniqueness: {case_sensitive: false}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true,
                     format: {with: VALID_EMAIL_REGEX},
